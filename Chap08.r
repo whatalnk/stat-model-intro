1c3b1a3a83303917fb5414146ad9e1f38e26e1d4
Wataru NAKAI <watnk0806@gmail.com>
2016-11-15 19:15:55 +0900

    Chapter 8

--- Chap08.r
+++ Chap08.r
@@ -18,8 +18,6 @@ ggplot(data_frame(x = c(0, 8)), aes(x)) +
                   args = list(size = 8, prob = 0.45), shape = 21, fill = "white") + 
     xlab(expression("y"[i]))
 
-choose(8, 2)
-
 logL <- function(q, y){
     sum(y * log(q) + (8 - y) * log(1 - q) + log(choose(8, y)))
 }
@@ -34,7 +32,7 @@ data_frame(q = seq(0.2, 0.7, 0.01)) %>>%
 
 73 / 160
 
-step_logL <- function(q, L){
+step_logL <- function(q, L, data){
     nq <- ifelse(sample(c(0, 1), size = 1, prob = c(0.5, 0.5)) == 0, q - 0.01, q + 0.01)
     nL <- logL(nq, data)
     if (nL > L){
@@ -44,19 +42,19 @@ step_logL <- function(q, L){
     }
 }
 
-do_step_logL <- function(start, nstep){
+do_step_logL <- function(start, nstep, data){
     qs <- numeric(nstep)
     qs[1] <- start
     pL <- logL(start, data)
     for(i in c(2:nstep)){
-        q_L <- step_logL(qs[i - 1], pL)
+        q_L <- step_logL(qs[i - 1], pL, data)
         qs[i] <- q_L$q
         pL <- q_L$L
     }
     return(qs)
 }
 
-data_frame(nstep = c(1:100), q03 = do_step_logL(0.3, 100), q06 = do_step_logL(0.6, 100)) %>>% 
+data_frame(nstep = c(1:100), q03 = do_step_logL(0.3, 100, data), q06 = do_step_logL(0.6, 100, data)) %>>% 
     gather(start, q, -nstep) %>>% 
     ggplot(aes(x = nstep, y = q, group = start, linetype = start)) + 
         geom_line() + 
@@ -66,7 +64,7 @@ data_frame(nstep = c(1:100), q03 = do_step_logL(0.3, 100), q06 = do_step_logL(0.
         ) + 
         scale_linetype(labels = c("0.30", "0.60"))
 
-step_metropolis <- function(q, L){
+step_metropolis <- function(q, L, data){
     nq <- ifelse(sample(c(0, 1), size = 1, prob = c(0.5, 0.5)) == 0, q - 0.01, q + 0.01)
     nL <- logL(nq, data)
     if (nL > L){
@@ -82,12 +80,12 @@ step_metropolis <- function(q, L){
     }
 }
 
-metropolis <- function(start, nstep){
+metropolis <- function(start, nstep, data){
     qs <- numeric(nstep)
     qs[1] <- start
     pL <- logL(start, data)
     for(i in c(2:nstep)){
-        q_L <- step_metropolis(qs[i - 1], pL)
+        q_L <- step_metropolis(qs[i - 1], pL, data)
         qs[i] <- q_L$q
         pL <- q_L$L
     }
@@ -98,26 +96,48 @@ library(gridExtra)
 
 options(repr.plot.width = 10, repr.plot.height = 2)
 
-# d.met1 <- data_frame(
-#     nstep = c(1:100), 
-#     q = metropolis(0.3, 100)
-# )
+step_metropolis(0.3, L = logL(0.4, data), data)
+
+metropolis(start = 0.3, nstep = 10, data = data)
+
+d.met1 <- data_frame(
+    nstep = c(1:100), 
+    q = metropolis(0.3, 100, data)
+)
 gp1 <- ggplot(data = d.met1, aes(x = nstep, y = q)) + geom_line() + ylim(c(0.25, 0.65))
-gp2 <- ggplot(data = d.met1, aes(q)) + geom_density() + xlim(c(0, 1))
+gp2 <- ggplot(data = d.met1, aes(q)) + geom_histogram(binwidth = 0.01) + xlim(c(0, 1))
 grid.arrange(gp1, gp2, ncol = 2)
 
-# d.met2 <- data_frame(
-#     nstep = c(1:1000), 
-#     q = metropolis(0.3, 1000)
-# )
+d.met2 <- data_frame(
+    nstep = c(1:1000), 
+    q = metropolis(0.3, 1000, data)
+)
 gp1 <- ggplot(data = d.met2, aes(x = nstep, y = q)) + geom_line() + ylim(c(0.25, 0.65))
-gp2 <- ggplot(data = d.met2, aes(q)) + geom_density() + xlim(c(0,1))
+gp2 <- ggplot(data = d.met2, aes(q)) + geom_histogram(binwidth = 0.01) + xlim(c(0,1))
 grid.arrange(gp1, gp2, ncol = 2)
 
-# d.met3 <- data_frame(
-#     nstep = c(1:100000), 
-#     q = metropolis(0.3, 100000)
-# )
+d.met3 <- data_frame(
+    nstep = c(1:100000), 
+    q = metropolis(0.3, 100000, data)
+)
 gp1 <- ggplot(data = slice(d.met3, seq(1, 100000, 100)), aes(x = nstep, y = q)) + geom_line() + ylim(c(0.25, 0.65))
-gp2 <- ggplot(data = slice(d.met3, seq(1, 100000, 100)), aes(q)) + geom_density() + xlim(c(0, 1))
+gp2 <- ggplot(data = slice(d.met3, seq(1, 100000, 100)), aes(q)) + geom_histogram(binwidth = 0.01) + xlim(c(0, 1))
 grid.arrange(gp1, gp2, ncol = 2)
+
+d.met4 <- data_frame(
+    nstep = c(1:500), 
+    q1 = metropolis(0.1, 500, data), 
+    q3 = metropolis(0.3, 500, data), 
+    q4 = metropolis(0.4, 500, data), 
+    q45 = metropolis(0.45, 500, data),    
+    q5 = metropolis(0.5, 500, data), 
+    q6 = metropolis(0.6, 500, data), 
+    q7 = metropolis(0.7, 500, data) 
+)
+
+options(repr.plot.width = 8, repr.plot.height = 4)
+
+d.met4 %>>% gather(start, q, -nstep) %>>% 
+    ggplot(aes(x = nstep, y = q, group = start, colour = start)) + 
+    geom_line() + 
+    scale_colour_discrete(labels = c("0.1", "0.3", "0.4", "0.45", "0.5", "0.6", "0.7"))
